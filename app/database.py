def81484e610ce7254a9b227550ca7838c075416
BayramAkdg <125415220+BayramAkdg@users.noreply.github.com>
2025-03-20 12:42:44 +0300

    database.py

--- app/database.py
+++ app/database.py
@@ -1,8 +1,22 @@
 from pymongo import MongoClient
 
-def get_database():
-    client = MongoClient("mongodb://mongo:27017/")
-    return client["library"], client
+class Database:
+    def __init__(self, uri="mongodb://localhost:27017/", db_name="library"):
+        self.client = MongoClient(uri)
+        self.db = self.client[db_name]
+        self.collection = self.db["books"]
 
-def close_connection(client):
-    client.close()
+    def insert(self, data):
+        self.collection.insert_one(data)
+
+    def find_all(self):
+        return list(self.collection.find({}, {"_id": 0}))
+
+    def update(self, filter_query, new_values):
+        self.collection.update_one(filter_query, {"$set": new_values})
+
+    def delete(self, filter_query):
+        self.collection.delete_one(filter_query)
+
+    def close(self):
+        self.client.close()
